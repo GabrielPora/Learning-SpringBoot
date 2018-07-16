d15fda3d667cedd96145a36aaecb38a87570880a
GabrielPora <gabriel.groener@gmail.com>
2018-07-16 08:13:16 +0200

    => Spring Beans & Dependency Injection => Spring Beans & Dependency Injection Demo

---
+++ springbeansdemo/src/test/java/com/beans/springbeansdemo/SpringBeansDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beans.springbeansdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBeansDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
