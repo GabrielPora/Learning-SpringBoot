a38088d7bc3764a3d041d83bc021008790ab2d8d
GabrielPora <gabriel.groener@gmail.com>
2018-07-16 13:45:00 +0200

    missed a small note

--- web-applications/controllers/src/main/java/com/therealdanvega/controller/ExceptionControllerAdvice.java
+++ web-applications/controllers/src/main/java/com/therealdanvega/controller/ExceptionControllerAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
-//	This is for global error handler 
+//	This is for global error handler
 	@ExceptionHandler(Exception.class)
 	public String exception(Exception exception, Model model){
 		model.addAttribute("exception", exception);
