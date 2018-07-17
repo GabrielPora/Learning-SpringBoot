a5851c9eaf99ac47b4864e24accd36241ceace87
GabrielPora <gabriel.groener@gmail.com>
2018-07-17 16:13:07 +0200

    Create

--- CRUD/initial/src/main/java/com/crudinit/config/WebConfig.java
+++ CRUD/initial/src/main/java/com/crudinit/config/WebConfig.java
@@ -1,12 +1,11 @@
 package com.crudinit.config;
 
+import com.crudinit.interceptor.BaseInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.crudinit.interceptor.BaseInterceptor;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
--- CRUD/initial/src/main/java/com/crudinit/service/PostService.java
+++ CRUD/initial/src/main/java/com/crudinit/service/PostService.java
@@ -1,41 +1,44 @@
 package com.crudinit.service;
 
-import java.util.List;
-
+import com.crudinit.domain.Post;
+import com.crudinit.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.crudinit.domain.Post;
-import com.crudinit.repository.PostRepository;
+import java.util.List;
 
 @Service
 public class PostService {
 
-	private PostRepository postRepository;
-	
-	@Autowired
-	public PostService(PostRepository postRepository){
-		this.postRepository = postRepository;
-	}
-	
-	public Post getLatestPost(){
-		return postRepository.findFirstByOrderByPostedOnDesc();
-	}
+    private PostRepository postRepository;
 
-	public List<Post> list() {
-		return postRepository.findAllByOrderByPostedOnDesc();
-	}
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 
-	public Post getBySlug(String slug) {
-		return postRepository.findBySlug(slug);
-	}
+    public Post getLatestPost() {
+        return postRepository.findFirstByOrderByPostedOnDesc();
+    }
 
-	public List<Post> listByAuthor(Long id) {
-		return postRepository.findAllByAuthorIdOrderByPostedOnDesc(id);
-	}
+    public List<Post> list() {
+        return postRepository.findAllByOrderByPostedOnDesc();
+    }
 
-	public Post get(Long id) {
-		return postRepository.findOne(id);
-	}
-	
+    public Post getBySlug(String slug) {
+        return postRepository.findBySlug(slug);
+    }
+
+    public List<Post> listByAuthor(Long id) {
+        return postRepository.findAllByAuthorIdOrderByPostedOnDesc(id);
+    }
+
+    public Post get(Long id) {
+        return postRepository.findOne(id);
+    }
+
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
 }
