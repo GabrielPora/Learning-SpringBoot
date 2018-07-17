eeb1cebae4a2a8dcb163d2207cf90faed78380bf
GabrielPora <gabriel.groener@gmail.com>
2018-07-17 16:30:38 +0200

    validation done

--- CRUD/initial/src/main/java/com/crudinit/service/PostService.java
+++ CRUD/initial/src/main/java/com/crudinit/service/PostService.java
@@ -1,44 +1,45 @@
 package com.crudinit.service;
 
-import com.crudinit.domain.Post;
-import com.crudinit.repository.PostRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.crudinit.domain.Post;
+import com.crudinit.repository.PostRepository;
 
 @Service
 public class PostService {
 
-    private PostRepository postRepository;
+	private PostRepository postRepository;
+	
+	@Autowired
+	public PostService(PostRepository postRepository){
+		this.postRepository = postRepository;
+	}
+	
+	public Post getLatestPost(){
+		return postRepository.findFirstByOrderByPostedOnDesc();
+	}
 
-    @Autowired
-    public PostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
+	public List<Post> list() {
+		return postRepository.findAllByOrderByPostedOnDesc();
+	}
 
-    public Post getLatestPost() {
-        return postRepository.findFirstByOrderByPostedOnDesc();
-    }
+	public Post getBySlug(String slug) {
+		return postRepository.findBySlug(slug);
+	}
 
-    public List<Post> list() {
-        return postRepository.findAllByOrderByPostedOnDesc();
-    }
+	public List<Post> listByAuthor(Long id) {
+		return postRepository.findAllByAuthorIdOrderByPostedOnDesc(id);
+	}
 
-    public Post getBySlug(String slug) {
-        return postRepository.findBySlug(slug);
-    }
+	public Post get(Long id) {
+		return postRepository.findOne(id);
+	}
 
-    public List<Post> listByAuthor(Long id) {
-        return postRepository.findAllByAuthorIdOrderByPostedOnDesc(id);
-    }
-
-    public Post get(Long id) {
-        return postRepository.findOne(id);
-    }
-
-    public Post save(Post post) {
-        return postRepository.save(post);
-    }
-
+	public Post save(Post post) {
+		return postRepository.save(post);
+	}
+	
 }
